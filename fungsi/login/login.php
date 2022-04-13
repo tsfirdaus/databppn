af2b3b1af78020ec4dcd3db9a8f4e6c1c75bc54e
tsfirdaus <tsfirdaus0@gmail.com>
2022-04-13 09:23:23 +0800

    style login admin

--- fungsi/login/login.php
+++ fungsi/login/login.php
@@ -1,51 +1,42 @@
 <?php 
-require '../../config.php';
+require 'config.php';
 if (isset($_POST["login"])){
 
 
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    mysqli_query($kon,"SELECT * FROM user WHERE username = '$username'");
-
+    mysqli_query($kon,"SELECT * FROM masuk WHERE username = '$username'");
 
     //cek username
     if(mysqli_num_rows($Result) === 1){
       //cek pass
-      $row = mysqli_fe
+      $row = mysqli_fetch_assoc($Result);
+      if ($password === $row["password"]){
+        header('location: ../../index.php');
+        exit;
+      }
+      
     }
 }
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Login Admin</title>
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <div class="center">
-      <h1>Login</h1>
-      <form method="post">
-        <div class="txt_field">
-          <input type="text" required>
-          <span></span>
-          <label>Username</label>
-        </div>
-        <div class="txt_field">
-          <input type="password" required>
-          <span></span>
-          <label>Password</label>
-        </div>
-        <div class="pass"></div>
-        <input type="submit" value="Login">
-        <div class="signup_link">
-          <a href="#"></a>
-        </div>
-      </form>
-    </div>
-
-  </body>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Login Admin</title>
+</head>
+<body>
+  <form action="" method="post">
+    <h1>login admin</h1>
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="username" placeholder="password">
+    <input type="submit" name="login" value="login">
+  </form>
+</body>
 </html>
